34486d7bb457be66f576ddf19dd156deef15bc18
Mxp <im@mxp.tw>
2015-04-26 16:53:34 +0800

    修正錯誤後發佈第二版

--- S3Backup.sample.py
+++ S3Backup.sample.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # Author Mxp.tw
 import os
-import time
+from datetime import datetime, timedelta
 
 # Variables
 backupfolderpath = '/PATH/TO/S3Backup'
 s3bucketpath = 's3://BUCKET/OBJECT'
-filestamp = time.strftime('%Y%m%d')
+filestamp = datetime.now().strftime('%Y%m%d')
 count = open(backupfolderpath+"/script/count.txt", "r").read().strip()
 days = 14 # 14D period
 counter = str(int(count)%days+1) 
-checkfiles = os.popen("ls %s/www | grep 'count-%s-*'" % (backupfolderpath, counter)).readline() 
+date_N_days_ago = datetime.now() - timedelta(days)
+filestamp_back = date_N_days_ago.strftime('%Y%m%d')
 
 # MySQL DB infomation
 username = '==DBNAME=='
@@ -36,30 +37,24 @@ databaselist.remove('information_schema\n')
 #... etc u know... again
 
 # remove old backup files
-if checkfiles.strip() != '':
-    os.popen("rm %s/mysql/count-%s-*" % (backupfolderpath,counter))
-    os.popen("rm %s/www/count-%s-*" % (backupfolderpath,counter))
-else:
-    print "Not Today! - %s" % (filestamp)
+os.popen("rm `ls %s/mysql/ | grep '%s'` 2> /dev/null" % (backupfolderpath,filestamp_back))
+os.popen("rm `ls %s/www/ | grep '%s'` 2> /dev/null" % (backupfolderpath,filestamp_back))
+# remove today's backup files (debug mode)
+os.popen("rm `ls %s/mysql/ | grep '%s'` 2> /dev/null" % (backupfolderpath,filestamp))
+os.popen("rm `ls %s/www/ | grep '%s'` 2> /dev/null" % (backupfolderpath,filestamp))
 
-if checkfiles.strip() == '':
+for database in databaselist:
+  database = database.strip()
+  filename = "%s/mysql/count-%s-%s-%s.sql" % (backupfolderpath, counter, database, filestamp)
+  print "Backing up %s" % (filename)
+  os.popen("mysqldump -u%s -p%s -h%s -e --opt -c %s | gzip -c -9 > %s.gz" % (username, password, hostname, database, filename))
+  print ".. done"
 
-  for database in databaselist:
-    database = database.strip()
-    filename = "%s/mysql/count-%s-%s-%s.sql" % (backupfolderpath, counter, database, filestamp)
-    print "Backing up %s" % (filename)
-    os.popen("mysqldump -u%s -p%s -h%s -e --opt -c %s | gzip -c -9 > %s.gz" % (username, password, hostname, database, filename))
-    print ".. done"
-
-  for www in wwwdir:
-    print "backing up www folder %s/%s" % (wwwpath, www)
-    os.popen("tar JcpfP %s/www/count-%s-%s-%s.tar.bz2 %s/%s" % (backupfolderpath, counter, www, filestamp, wwwpath, www))
+for www in wwwdir:
+  print "backing up www folder %s/%s" % (wwwpath, www)
+  os.popen("tar JcpfP %s/www/count-%s-%s-%s.tar.bz2 %s/%s" % (backupfolderpath, counter, www, filestamp, wwwpath, www))
 
   #update count.txt (S3Backup/script/count.txt)
   os.popen("echo %s > %s/script/count.txt" % (str((int(count)+1)), backupfolderpath))
   os.popen("s3cmd sync --delete-removed --reduced-redundancy --skip-existing %s/www/ %s/www/" % (backupfolderpath, s3bucketpath))
   os.popen("s3cmd sync --delete-removed --reduced-redundancy --skip-existing %s/mysql/ %s/mysql/" % (backupfolderpath, s3bucketpath))
-
-else:
-    print "Not Today! - %s" % (filestamp)
-
